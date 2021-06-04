e9c3c6ee701a16dbfa70c884889af92f3b868320
spencer <spencer@31prime.co.uk>
2021-06-04 16:06:08 +0100

    Inital

---
+++ rest-reactive-service/src/main/java/com/example/restreactiveservice/RestReactiveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.restreactiveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestReactiveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestReactiveServiceApplication.class, args);
+    }
+
+}
